8d91515010839dba391c98072450d471f47bba89
Tishana <tishana.trainor@gmail.com>
2018-12-17 09:40:53 -0500

    methods complete

--- methods.py
+++ methods.py
@@ -1,16 +1,25 @@
 # Create the following methods and make sure they return the correct data type
 # and/or value
 
+
 # #1: Create a method called num_list_with_arg that takes a positive integer and
 # returns a list of integers between 1 and the number passed in.
 #
 # So if the number 5 is passed in, num_list_with_arg should return [1, 2, 3, 4]
+def num_list_with_arg(num):
+    new_list = []
+    i = 0
+    while i < num:
+        new_list.append(i)
+        i += 1
+    print(new_list)
 
 
 # #2: Modify the has_ruby_exp method below so that it returns a SORTED list of
 # all instructors who have Ruby experience (i.e. where "ruby" == True)
 # The list should contain only names of instructors.
 # make sure you name the list ruby_experience before returning it.
+
 
 def has_ruby_exp():
     ruby_experience = []
@@ -49,7 +58,11 @@ def has_ruby_exp():
             'pasta': False
         }
     }
-    
+    for x in experience:
+        if x['ruby'] == True:
+            ruby_experience.append(x)
+
+    print(ruby_experience)
 
 
 # #3: Create a method called toggle_str_num that takes an argument. If the
@@ -57,6 +70,16 @@ def has_ruby_exp():
 # argument is an integer, convert it to a string and return the string; If the
 # argument is neither a string nor an integer, return the string "this is not a
 # str or a int":
+
+
+def toggle_str_num(x):
+    if type(x) == 'str':
+        y = x.replace("'", '')
+        print(y)
+    elif type(x) == 'int':
+        print(f'{x}')
+    else:
+        print("this is not a str or an int")
 
 
 # Commit when you finish working on these questions!
